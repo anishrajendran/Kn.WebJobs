b11c5c12ab7eba6f08d0062f6663dec4905818d7
Anish R <Anish_Rajendran@infosys.com>
2017-09-04 08:24:18 +0530

    Added Webjob

--- Kn.WebJobs/Module1.vb
+++ Kn.WebJobs/Module1.vb
@@ -1,7 +1,15 @@
-﻿Module Module1
+﻿Imports System.Net.Http.HttpClient
+Imports System.Net.Http
+Imports RestSharp
+
+Module Module1
 
     Sub Main()
-
+        Console.WriteLine("WebJob started!!!")
+        Dim _client As New RestSharp.RestClient("http://kynetapp.azurewebsites.net/")
+        Dim startAsyncRequest = New RestSharp.RestRequest("api/RatesKeeper/ExtractPricesFromExchanges/", Method.GET)
+        Dim startResponce = _client.Execute(startAsyncRequest)
+        Console.WriteLine("WebJob completed!!!")
     End Sub
 
 End Module
